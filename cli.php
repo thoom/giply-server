f430bd4c1b435fdc34d19b7892f5940ca6b68d4e
zdp <zdp@thoomtech.com>
2012-11-08 10:42:59 -0700

    Simplified cli script params

--- cli.php
+++ cli.php
@@ -2,8 +2,20 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$action = $argv[1];
+$action      = $argv[1];
 $project_dir = $argv[2];
+
+if (!is_dir($project_dir)) {
+    $project_dir = dirname(__DIR__) . "/$project_dir";
+}
+
+if (!is_dir($project_dir)) {
+    exit("Invalid project directory");
+}
+
+if (!is_dir($project_dir . "/.git")) {
+    exit("Missing required Git repository");
+}
 
 $deploy = new Thoom\Giply($project_dir);
 $deploy->$action();
